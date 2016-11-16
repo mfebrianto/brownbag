5c34f3029e4848c4958dae1f1b9d2adf4a12f351
michael febrianto <michaelfebrianto@gmail.com>
2016-11-16 19:29:20 +1100

    master : getMetaData done

--- index.js
+++ index.js
@@ -5,8 +5,9 @@ var cookie;
 
 console.log("start")
 
+
 function readCookie() {
-  var content =fs.readFileSync('cookie.txt', 'utf8', function (err,data) {
+  var content =fs.readFileSync('cookies.txt', 'utf8', function (err,data) {
     if (err) {
       return console.log(err);
     }
@@ -15,8 +16,39 @@ function readCookie() {
   return content;
 }
 
-function getFile() {
-  var content = fs.readFileSync('createSearchJob.json', 'utf8', function (err,data) {
+
+function getOptions(path, method, cookie){
+  var uname='su0I2qPWVZm1cu';
+  var pword='3c2YfyW4r3QxvJK0eDMNYIWMD7O9OiasMMTh2jWtSO4FBrjAf4vrIQIsD9cP9Z95';
+
+  var headers = {
+    'Content-type': 'application/json',
+    'Accept': 'application/json',
+    'Authorization': 'Basic ' + new Buffer(uname + ':' + pword).toString('base64')
+  };
+
+  if (cookie != null && cookie != undefined){
+    headers = Object.assign({'cookie': cookie},headers)
+  }
+
+  return {
+    host: 'api.au.sumologic.com',
+    port: '443',
+    path: path,
+    method: method,
+    headers: headers
+  };
+}
+
+
+function getFile(file) {
+  if (file == null || file == undefined) {
+    file = 'createSearchJob.json'
+  }
+
+  console.log(file);
+
+  var content = fs.readFileSync(file, 'utf8', function (err,data) {
     if (err) {
       return console.log(err);
     }
@@ -26,7 +58,7 @@ function getFile() {
 }
 
 function writeResult(data){
-  fs.writeFileSync("result.json", data.toString())
+  fs.writeFileSync("result.json", data.toString());
 }
 
 function writeFile(res) {
@@ -35,37 +67,23 @@ function writeFile(res) {
   fs.writeFileSync("cookies.txt", cookie)
 }
 
-function executeQuery(data){
-  console.log("executeQuery");
-  console.log(data.toString());
-}
+function getMetaData() {
+  var queryId = JSON.parse(getFile('result.json'))['id'];
+
+  var getReq = https.request(getOptions('/api/v1/search/jobs/'+queryId,'GET', readCookie()), function(res) {
+    res.on('data', function (data) {
+      console.log(data);
+    });
+
+  });
+};
 
 function getTheLogs() {
-  console.log("inside the function");
-
-  var uname='su0I2qPWVZm1cu';
-  var pword='3c2YfyW4r3QxvJK0eDMNYIWMD7O9OiasMMTh2jWtSO4FBrjAf4vrIQIsD9cP9Z95';
-
-  var options = {
-    host: 'api.au.sumologic.com',
-    port: '443',
-    path: '/api/v1/search/jobs',
-    method: 'POST',
-    headers: {
-      'Content-type': 'application/json',
-      'Accept': 'application/json',
-      'Authorization': 'Basic ' + new Buffer(uname + ':' + pword).toString('base64')
-    }
-  };
-
-  console.log("searchJob");
-  console.log(getFile());
-
-  var getReq = https.request(options, function(res) {
+  var getReq = https.request(getOptions('/api/v1/search/jobs','POST'), function(res) {
     res.on('data', function (data) {
       writeFile(res);
       writeResult(data);
-      executeQuery();
+      getMetaData();
     });
 
     res.on('end', function(){
@@ -78,9 +96,5 @@ function getTheLogs() {
 
   return true;
 };
-
-function executeQuery(){
-  console.log("executeQuery");
-}
 
 getTheLogs();
